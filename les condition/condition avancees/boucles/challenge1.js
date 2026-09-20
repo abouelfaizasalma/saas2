b492e440b4dff49ec99b9144fdc61a6888b458a1
salmaabouelfaiza <abouelfaizasalma189@gmail.com>
2026-09-20 21:41:04 +0200

    challenge tableaux 1

--- les condition/condition avancees/boucles/challenge1.js
+++ les condition/condition avancees/boucles/challenge1.js
@@ -2,8 +2,7 @@ const prompt = require('prompt-sync')();
 let N = parseFloat(prompt("entrer un nombre:"));
 
 for ( let i = 1 ; i <= 10 ; i++) {
-     resultat = N * i
+     let resultat = N * i
 
 console.log(`${N} *${i} = ${resultat}`);
  }
-
