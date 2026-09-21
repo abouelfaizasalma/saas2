6ba634dd254ffe014c526b15b8bb974313f27c37
salmaabouelfaiza <abouelfaizasalma189@gmail.com>
2026-09-21 17:35:15 +0200

    challege1

---
+++ boucles/chaines/challenge1.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+let texte =  prompt("Entrer un  texte : ");
+
+
+console.log(" tape :" + texte );
+
+
+
+
---
+++ boucles/chaines/challenge2.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+let texte = prompt("Eentre un chaine : ");
+let compture = 0 ;
+
+for (let i = 0 ; i >= texte.length ; i++ ); {
+    compture++
+}
+console.log( compture );
+
+
